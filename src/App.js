dff89895f00c5e0f8f21ce74b07b024303a60ad9
mohibuslc <mohib_uslc@outlook.com>
2021-06-18 23:04:00 +0100

    Dom+algorithom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const Info = ['Karim ', 'Rahim', 'Tina'];
+  const products = [
+    
+    {name:'Chiken', price: '$200'},
+    {name:'lam', price:'$230'},
+    {name:'Cow', price:'$3000' }
+    
+
+]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <MovieCounter></MovieCounter>
+        <Product prd={products[0]}></Product>
+
+        <Product prd={products[1]}></Product>
+        <Product prd={products[2]}></Product>
+        
+        <ul>
+       {
+
+        
+       Info.map(ActInfo => <li>{ActInfo}</li>)
+
+         
+       }
+       </ul>
       </header>
     </div>
   );
+}
+
+function Nayok (props){
+
+  return(
+
+    <div>
+
+      <h1> Nayoks: {props.ActInfo}</h1>
+    </div>
+  )
+}
+function Product(props){
+  const ProductStyle={
+    border:'2px solid red',
+    margin:'10px',
+    padding: '5px',
+    width:'900px',
+    height:'155px'
+
+  }
+const {name ,price} =props.prd
+  return(
+
+    <div style={ProductStyle}>
+
+      <h2>product name : {name}</h2>
+      <h3>PRODUCT_Price: {price}</h3>
+    </div>
+  )
+}
+// movie counter : 
+
+function MovieCounter(){
+const [count , setCount]=useState(0);
+const handleincrase =()=>setCount(count+1);
+const handleDecrase =()=>setCount(count-1);
+return(
+
+  <div>
+
+  <h3>Movie_Count:{count}</h3>
+  <h3>Movie_Count:{count+4}</h3>
+  <h3>Movie_Count:{count-3}</h3>
+
+  <button onClick ={handleincrase}>Incrase Movies </button>
+  <button onClick ={handleDecrase}>Decrase Movies </button>
+
+  </div>
+)
+
 }
 
 export default App;
